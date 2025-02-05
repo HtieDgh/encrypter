1a81d44ebf1871e687a325da7be34775432d5771
HtieDgH <54172462+HtieDgh@users.noreply.github.com>
2025-02-05 21:49:47 +0300

    refactor: добавлена директория для автоматических тестов

---
+++ test/UnitTest/UnitTest.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\..\src\encrypt\Ceasar.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
